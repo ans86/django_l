0a42f88bd2f639c75eca6698804308dbf820c766
Ans <ansmayo86@gmail.com>
2025-08-08 04:56:28 +0500

    Update Everything

--- car/urls.py
+++ car/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import car
+from .views import car, car_detail,car_edit,car_delete
 
 urlpatterns = [
     path('', car, name='car'),
+    path('car/<int:id>/', car_detail, name='car_detail'),
+    path('car/edit/<int:id>/', car_edit, name='car_edit'),
+    path('car/delete/<int:id>/', car_delete, name='car_delete')
 ]
